8e705d810bd2f186624356b28f1ceafa4ec30cf0
jyoti <jyotidhage10@gmail.com>
2022-08-07 11:07:01 +0530

    [Aaradhya]Add:Welcome to line comparison computation

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComparision1.LengthOfOneline();
+            Linecomparison2.LengthOfTwoLines();
+            LineComparison3.CompareTwoLines();
+
+
+
+
+        }
+    } 
+}
